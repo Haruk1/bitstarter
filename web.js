86581ede64747456614469607fb94660586513f1
haruki <haruki@ubuntu>
2013-07-11 09:39:14 +0200

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -1,11 +1,21 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+
+// constants
+var INDEX_HTML = "index.html";
+
+
+// request handlers
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var buf = fs.readFileSync(INDEX_HTML);
+    response.send(buf.toString());
 });
 
+
+// server
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
